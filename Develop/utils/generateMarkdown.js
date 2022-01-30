3bef7e075a3e79e6160ee69adbdbaf929389cd50
Nate-Huerd <nhuerd3@gmail.com>
2022-01-29 20:32:21 -0600

    added licensing badge template to update later

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,16 +1,80 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-
 // watch video for guide to linking badges: https://egghead.io/lessons/javascript-how-to-write-a-javascript-library-adding-badges-to-your-readme
+
 function renderLicenseBadge(license) {
   let badge = '';
   // add badges to licenses (if license equals ___, (link to badge))
-  if (license === 'Apache License 2.0')(//link to Apache License 2.0 badge)
-}
+  if (license === 'Apache License 2.0') {
+    badge = '[![License: Apache 2.0](//link to Apache License 2.0 badge)]';
+  } else if 
+  (license === '') {
+    badge = '[![License: _____](link)]';
+  } else if
+  (license === '') {
+    badge = '[![License: _____](link)]';
+  } else if
+  (license === '') {
+    badge = '[![License: _____](link)]';
+  } else if
+  (license === '') {
+    badge = '[![License: _____](link)]';
+  } else if
+  (license === '') {
+    badge = '[![License: _____](link)]';
+  } else if
+  (license === '') {
+    badge = '[![License: _____](link)]';
+  } else if
+  (license === '') {
+    badge = '[![License: _____](link)]';
+  } else if
+  (license === '') {
+    badge = '[![License: _____](link)]';
+  } else if
+  (license === '') {
+    badge = '[![License: _____](link)]';
+  } else if
+  (license === '') {
+    badge = '[![License: _____](link)]';
+  }
+  return badge;
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === '') {
+    return //link to badge
+  } else if
+  (license === '') {
+    return //link to badge
+  } else if
+  (license === '') {
+    return //link to badge
+  } else if
+  (license === '') {
+    return //link to badge
+  } else if
+  (license === '') {
+    return //link to badge
+  } else if
+  (license === '') {
+    return //link to badge
+  } else if
+  (license === '') {
+    return //link to badge
+  } else if
+  (license === '') {
+    return //link to badge
+  } else if
+  (license === '') {
+    return //link to badge
+  } else if
+  (license === 'null') {
+    return '';
+  }
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
