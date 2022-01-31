a1cb9254b81a267d5b17015d30543c10cfc95199
Nate-Huerd <nhuerd3@gmail.com>
2022-01-30 18:12:31 -0600

    changed link = to return, otherwise it wouldn't return the link

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -56,43 +56,43 @@ function renderLicenseBadge(license) {
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license === 'Apache License 2.0') {
-    link = '(https://opensource.org/licenses/Apache-2.0)';
+    return '(https://opensource.org/licenses/Apache-2.0)';
   } else if 
   (license === 'GNU General Public License') {
-    link = '(https://www.gnu.org/licenses/gpl-3.0)';
+    return '(https://www.gnu.org/licenses/gpl-3.0)';
   } else if
   (license === 'MIT License') {
-    link = '(https://opensource.org/licenses/MIT)';
+    return '(https://opensource.org/licenses/MIT)';
   } else if
   (license === 'BSD 2-Clause "Simplified" License') {
-    link = '(https://opensource.org/licenses/BSD-2-Clause)';
+    return '(https://opensource.org/licenses/BSD-2-Clause)';
   } else if
   (license === 'BSD 3-Clause "New" or "Revised" License') {
-    link = '(https://opensource.org/licenses/BSD-3-Clause)';
+    return '(https://opensource.org/licenses/BSD-3-Clause)';
   } else if
   (license === 'Boost Software License 1.0') {
-    link = '(https://www.boost.org/LICENSE_1_0.txt)';
+    return '(https://www.boost.org/LICENSE_1_0.txt)';
   } else if
   (license === 'Creative Commons Zero v1.0 Universal') {
-    link = '(http://creativecommons.org/publicdomain/zero/1.0/)';
+    return '(http://creativecommons.org/publicdomain/zero/1.0/)';
   } else if
   (license === 'Eclipse Public License 2.0') {
-    link = '(https://opensource.org/licenses/EPL-2.0)';
+    return '(https://opensource.org/licenses/EPL-2.0)';
   } else if
   (license === 'GNU Affero General Public License v3.0') {
-    link = '(https://www.gnu.org/licenses/agpl-3.0)';
+    return '(https://www.gnu.org/licenses/agpl-3.0)';
   } else if
   (license === 'GNU General Public License v2.0') {
-    link = '(https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)';
+    return '(https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)';
   } else if
   (license === 'GNU Lesser General Public License v2.1') {
-    link = '(https://www.gnu.org/licenses/lgpl-2.1)';
+    return '(https://www.gnu.org/licenses/lgpl-2.1)';
   } else if
   (license === 'Mozilla Public License 2.0') {
-    link = '(https://opensource.org/licenses/MPL-2.0)';
+    return '(https://opensource.org/licenses/MPL-2.0)';
   } else if
   (license === 'The Unlicense') {
-    link = '(https://img.shields.io/badge/licen(http://unlicense.org/)';
+    return '(https://img.shields.io/badge/licen(http://unlicense.org/)';
   } else if
   // need to account for "No License" again here
   (license === 'No License') {
